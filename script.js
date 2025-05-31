2a9a8f3ad1c5461c87d30c39dbab45b9dc63928f
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-05-31 12:45:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const output=document.querySelector("#output");
 function myPromise(){
 	return new Promise((resolve)=>{
             setTimeout(()=>{
@@ -10,13 +11,14 @@ function myPromise(){
 	.then((arr)=>{
 		setTimeout(()=>{
 			const evens=arr.filter(nums=>nums%2===0)
-			const arr1=evens.join(",");
-			resolve(arr1);
+			output.textcontent=evens.join(",");
+			resolve(evens);
 		},1000)
 	})
 	.then((arr)=>{
 		setTimeout(()=>{
 			const ans=arr.map(nums=>nums*2);
+			output.textcontent=ans.join(",");
 			resolve(ans);
 		},2000)
 	})
